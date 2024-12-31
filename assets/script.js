bfbee2fe4dd70535b17c6fac8eae9f92b0cc9ca9
Dalet <104555136+Spod101@users.noreply.github.com>
2024-12-31 16:44:34 +0800

    first push

--- assets/script.js
+++ assets/script.js
@@ -5,8 +5,5 @@ function sendEmail() {
         subject: "Love Proposal",
     }
 
-    emailjs.send('service_lyhluhf', 'template_ei6cfei', params).then(function(response) {
-        console.log('SUCCESS!', response.status, response.text);
-        alert('Email sent successfully!');
+    emailjs.send('service_lyhluhf', 'template_ei6cfei', params)
     }
-}
